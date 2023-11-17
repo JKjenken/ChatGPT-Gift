ab5a1feb7d290a4509a64b90c524f92dea1d0301
linjiankai <jk.jenken@outlook.com>
2023-11-17 08:39:45 +0800

    fix 模型名称异常

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -501,7 +501,7 @@ export const useChatStore = create<ChatStore>()(
           api.llm.chat({
             messages: topicMessages,
             config: {
-              model: "gpt-3.5-turbo",
+              model: "gpt-3.5-turbo-1106",
             },
             onFinish(message) {
               get().updateCurrentSession(
@@ -555,7 +555,7 @@ export const useChatStore = create<ChatStore>()(
                 date: "",
               }),
             ),
-            config: { ...modelConfig, stream: true, model: "gpt-3.5-turbo" },
+            config: { ...modelConfig, stream: true, model: "gpt-3.5-turbo-1106" },
             onUpdate(message) {
               session.memoryPrompt = message;
             },
